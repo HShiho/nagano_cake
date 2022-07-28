0979f229d1423aa8840823dd129c6d0648d890a7
HShiho <108495008+HShiho@users.noreply.github.com>
2022-07-28 15:41:50 +0000

    [create]商品詳細

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,19 @@
 module ApplicationHelper
-  
+
+    class << self
+
+      def digit_price(price)
+        "#{price.to_s(:delimited, delimiter: ',' )}"
+      end
+
+      def tax_price(price)
+        amount = price * 1.1
+        tax = amount.floor
+        "#{tax.to_s(:delimited, delimiter: ',' )}"
+      end
+      
+      
+
+    end
+
 end
