dd2184abc8735eb9ac682e1aac2cb1b95b20deef
HShiho <108495008+HShiho@users.noreply.github.com>
2022-07-28 13:56:14 +0000

    [add]一覧の価格表示を桁区切り仕様に

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
- 
+
   # 管理者用 URL/admin/sign_in...
   devise_for :admin, skip:[:registrations, :passwords] ,controllers:{
     sessions:"admin/sessions"
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     root to: 'homes#top'
+
     resources :genres
     resources :items
   end
