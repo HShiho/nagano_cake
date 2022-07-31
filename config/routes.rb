351e4374de51864954d55fdb03ba2044e222b52f
HShiho <108495008+HShiho@users.noreply.github.com>
2022-07-31 13:48:30 +0000

    [add]注文履歴一覧画面enum実装

--- config/routes.rb
+++ config/routes.rb
@@ -12,12 +12,13 @@ Rails.application.routes.draw do
   }
 
   namespace :admin do
-    root :to => 'homes#top'
 
     resources :genres
     resources :items
     resources :customers,except: [:new, :create]
     resources :orders,except: [:new, :create, :delete, :edit]
+
+    root :to => 'homes#top'
 
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_31_114720) do
+ActiveRecord::Schema.define(version: 2022_07_31_121922) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -108,6 +108,8 @@ ActiveRecord::Schema.define(version: 2022_07_31_114720) do
     t.integer "manufacture_status", default: 0, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "item_id", null: false
+    t.integer "order_id", null: false
   end
 
   create_table "orders", force: :cascade do |t|
