ab3f3a75f7a0c0abd31356dabe4a5767697bc4bb
HShiho <108495008+HShiho@users.noreply.github.com>
2022-08-07 13:00:38 +0000

    [create]登録情報編集ページ作成

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -8,5 +8,25 @@ class Public::CustomersController < ApplicationController
 
   def edit
     @current_customer = current_customer
+    @customer = Customer.find(params[:id])
   end
+
+  def update
+     @customer = Customer.find(params[:id])
+     @customer.update(customer_params)
+     redirect_to public_customer_path(@customer.id)
+  end
+
+  def withdrawal
+    @current_customer = current_customer
+  end
+
+
+
+   private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -28,6 +28,7 @@ Rails.application.routes.draw do
   namespace :public do
 
     get '/about' => 'homes#about',as: 'about'
+    get '/customers/withdrawal' => 'customers#withdrawal',as: 'withdrawal'
     resources :customers,except: [:new, :index, :delete]
     resources :cart_items,except: [:new, :show, :edit]
     resources :items,except: [:new, :create, :delete, :edit]
