29ec36ce0a70c5baefa177718d0b8c1628563da8
HShiho <108495008+HShiho@users.noreply.github.com>
2022-08-08 05:48:51 +0000

    [create]配送先一覧ページ作成

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,9 +1,35 @@
 class Public::AddressesController < ApplicationController
+  layout 'public'
+
   def index
     @current_customer = current_customer
+    @address = Address.new
+    @addresses = Address.where(customer_id: "#{current_customer.id}")
+  end
+
+  def create
+    @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
+    @address.save
+    redirect_to public_addresses_path
   end
 
   def edit
     @current_customer = current_customer
+    @address = Address.find(params[:id])
   end
+
+  def destroy
+    @address = Address.find(params[:id])
+    @address.destroy
+    redirect_to public_addresses_path
+  end
+
+
+   private
+
+  def address_params
+    params.require(:address).permit(:customer_id, :name, :postal_code, :address)
+  end
+
 end
