f36938006bc8ef3eba3fb87c6bdce7682b82f1b8
HShiho <108495008+HShiho@users.noreply.github.com>
2022-08-08 15:50:23 +0000

    [create]商品一覧ページ作成

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -2,6 +2,7 @@ class Public::HomesController < ApplicationController
   layout 'public'
 
   def top
+    @current_customer = current_customer
     @genres = Genre.all
     @items = Item.limit(4).order("created_at DESC")
   end
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,9 +1,21 @@
 class Public::ItemsController < ApplicationController
   layout 'public'
-  
+
   def index
+    @current_customer = current_customer
+    @genres = Genre.all
+
+    if params[:genre_id]
+      @genre = Genre.find(params[:genre_id])
+      @items = @genre.items.order("created_at DESC").all
+    else
+      @items = Item.order("created_at DESC").all
+    end
+
   end
 
   def show
+    @current_customer = current_customer
   end
+
 end
