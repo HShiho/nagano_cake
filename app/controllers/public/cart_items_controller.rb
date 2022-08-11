11954ac4b463c24f842c3cc2641d9bbf06db617b
HShiho <108495008+HShiho@users.noreply.github.com>
2022-08-11 14:01:59 +0000

    [add]カートページupdateアクション設定

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -22,12 +22,17 @@ class Public::CartItemsController < ApplicationController
     end
   end
 
+  def update
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.update(cart_item_params)
+    redirect_to public_cart_items_path
+  end
+
 
   private
 
   def cart_item_params
-    # params.require(:cart_item).permit(:customer_id, :item_id, :amount)
-    params.permit(:customer_id, :item_id, :amount)
+    params.require(:cart_item).permit(:customer_id, :item_id, :amount)
   end
 
 end
