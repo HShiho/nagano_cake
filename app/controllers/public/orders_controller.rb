85e4205d13555f250f406da139b14928c147b470
HShiho <108495008+HShiho@users.noreply.github.com>
2022-08-14 00:35:50 +0000

    [add]注文情報入力ページ住所選択欄追加

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -4,10 +4,18 @@ class Public::OrdersController < ApplicationController
   def new
     @current_customer = current_customer
     @order = Order.new
+    @address = Address.where(customer_id:["#{@current_customer.id}"])
     if request.post?
       if params["my_address"]
+        @order.postal_code = current_customer.postal_code
+        @order.address = current_customer.address
+        @order.name = "#{current_customer.last_name + current_customer.first_name}"
       elsif params["select_address"]
+        @order.postal_code = Address.find(params[:order][:select_address]).postal_code
+        @order.address = Address.find(params[:order][:select_address]).address
+        @order.name = Address.find(params[:order][:select_address]).name
       else params["new_address"]
+        @order = @order.new(params[:new_address])
       end
     end
   end
@@ -16,7 +24,6 @@ class Public::OrdersController < ApplicationController
     @current_customer = current_customer
     @order = Order.new(order_params)
     render :new if @order.invalid?
-
   end
 
   def create
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,10 @@
 class Address < ApplicationRecord
-  
+
   belongs_to :customer
-  
+
+  def select_register_addresses
+    "〒" + self.postal_code + " " + self.address + "　" + self.name
+  end
+
+
 end
