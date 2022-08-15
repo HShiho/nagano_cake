bbe4da45fe323f7204e2ea20769a16028ee410c6
HShiho <108495008+HShiho@users.noreply.github.com>
2022-08-15 14:57:58 +0000

    [add]注文情報確認ページ支払い方法欄作成

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -6,29 +6,32 @@ class Public::OrdersController < ApplicationController
     session.delete(:order)
     @order = Order.new
     # @address = Address.where(customer_id:["#{@current_customer.id}"])
-
   end
 
   def confirm
     @order = Order.new(order_params)
     session[:order] = @order
 
-      if params[:order][:address_number] == "1"
+      if params[:address_number] == "1"
         @order.postal_code = current_customer.postal_code
         @order.address = current_customer.address
-        @order.name = "#{current_customer.last_name + current_customer.first_name}"
-      elsif params[:order][:address_number] == "2"
-        @order.postal_code = Address.find(params[:order][:register]).postal_code
-        @order.address = Address.find(params[:order][:register]).address
-        @order.name = Address.find(params[:order][:register]).name
-      else params[:order][:address_number] == "3"
-        @order = Order.new(address_params)
-      # else
-        # redirect_to new_public_order_path
+        @order.name = ApplicationHelper.full_name(current_customer.last_name , current_customer.first_name)
+      elsif params[:address_number] == "2"
+        @order.postal_code = Address.find(params[:register]).postal_code
+        @order.address = Address.find(params[:register]).address
+        @order.name = Address.find(params[:register]).name
+      elsif params[:address_number] == "3"
+        address_new = Address.new(address_params)
+        address_new.customer_id = current_customer.id
+      else
+        @order.postal_code = current_customer.postal_code
+        @order.address = current_customer.address
+        @order.name = ApplicationHelper.full_name(current_customer.last_name , current_customer.first_name)
       end
 
     @cart_items = current_customer.cart_items.all
     @total = @cart_items.inject(0) { |sum, item| sum + item.sum_of_price }
+    @postage = 800
     # render :new if @order.invalid?
   end
 
@@ -60,7 +63,7 @@ private
   end
 
   def address_params
-    params.require(:order).permit(:postal_code, :address, :name)
+    params.require(:address).permit(:customer_id, :name, :postal_code, :address)
   end
 
 end
