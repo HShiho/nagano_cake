7ed3102be956d02173cfc30b81dae5848281682a
HShiho <108495008+HShiho@users.noreply.github.com>
2022-08-17 16:06:43 +0000

    [fix]金額表示欄修正

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -1,19 +1,20 @@
 class Admin::OrderItemsController < ApplicationController
-  
+
   def update
     @order = Order.find(params[:id])
-    @order_item = Order_item.find(@order.order_item.id)
+    @order_item = @order.order_items.find_by(item_id: params[:item_id])
     @order_item.update(order_item_params)
     redirect_to admin_order_path(@order.id)
-    
+
   end
-  
-  
-  
+
+
+
    private
 
   def order_item_params
-    params.require(:order_item).permit(:manufacture_status)
+    params.permit(:manufacture_status)
+    # params.require(:order_item).permit(:manufacture_status)
   end
-  
+
 end
