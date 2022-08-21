591de1bb3d6aa892520665265a7e6abc571eb193
HShiho <108495008+HShiho@users.noreply.github.com>
2022-08-21 17:24:59 +0000

    [fix]ステータス連動実装のエラー修正

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -2,13 +2,15 @@ class Admin::OrderItemsController < ApplicationController
 
   def update
     @order_item = OrderItem.find(params[:id])
-    if @order_item.update(order_item_params)
     @order_id = @order_item.order_id
-    @order_items = OrderItem.where(order_id: "#{@order}")
-    @order_items.update(order_status: "production") if @order_item.manufacture_status == "m_production"
+    @order = Order.find("#{@order_id}")
+    @order_item.update(order_item_params)
+    if @order_item.manufacture_status == "m_production"
+      @order.update(order_status: "production")
+    elsif @order_item.manufacture_status == "complete" and ( @order.order_items.where(manufacture_status: "complete").count == @order.order_items.count )
+      @order.update(order_status: "shopping_preparation")
     end
     redirect_to admin_order_path(@order_id)
-
     # @order_item = OrderItem.find(params[:id])
     # @order_item.update(order_item_params)
     # @order = @order_item.order_id
